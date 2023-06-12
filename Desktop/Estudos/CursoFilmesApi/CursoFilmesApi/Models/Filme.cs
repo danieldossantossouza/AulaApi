4a528aa4f7ba35fa4d4eed2f57ebfe84e2c8de02
danieldossantossouza <danieldossantossouza.88gmail.com>
2023-06-12 17:25:11 -0300

    tanbalhando com campos obrigatorios.

--- Desktop/Estudos/CursoFilmesApi/CursoFilmesApi/Models/Filme.cs
+++ Desktop/Estudos/CursoFilmesApi/CursoFilmesApi/Models/Filme.cs
@@ -1,9 +1,18 @@
-﻿namespace CursoFilmesApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoFilmesApi.Models
 {
     public class Filme
     {
-        public string Titulo { get; set; }
-        public string Genero { get; set; }
+        [Required(ErrorMessage= "O título do filme é obrigatorio !")]
+        public string? Titulo { get; set; }
+
+        [Required(ErrorMessage = "O gênero do filme é obrigatorio !")]
+        [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder a 50 caracteres. ")]
+        public string? Genero { get; set; }
+
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos.")]
         public int Duracao { get; set; }
     }
 }
